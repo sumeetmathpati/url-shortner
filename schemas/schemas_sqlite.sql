6c56da8f9b61e20f7501d47280dc7b38be6a3a91
Sumeet <sumeet221b@gmail.com>
2020-10-25 08:07:57 +0530

    added sqlite 3 schema

---
+++ schemas/schemas_sqlite.sql
@@ -0,0 +1,21 @@
+CREATE TABLE users (
+    id int primary key autoincrement,
+    username text not null unique,
+    password text not null,
+    admin boolean not null
+);
+
+CREATE TABLE public.urls
+(
+    id int primary key autoincrement,
+    short_url text COLLATE pg_catalog."default" NOT NULL,
+    long_url text COLLATE pg_catalog."default" NOT NULL,
+    user_id bigint,
+    url_name text COLLATE pg_catalog."default",
+    CONSTRAINT urls_pkey PRIMARY KEY (id),
+    CONSTRAINT urls_long_url_user_id_key UNIQUE (long_url, user_id),
+    CONSTRAINT urls_user_id_fkey FOREIGN KEY (user_id)
+        REFERENCES public.users (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+)
