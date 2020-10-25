a86129cfcfa50af1378a352b035ca3179c4bfbe0
Sumeet <sumeet221b@gmail.com>
2020-10-25 16:39:04 +0530

    added sqlite config to database.py

--- database.py
+++ database.py
@@ -2,6 +2,9 @@ from flask import g
 import psycopg2
 from psycopg2.extras import DictCursor
 from .env import POSTGRES_URL
+import sqlite3
+
+# Postgres
 
 def connect_db():
     conn = psycopg2.connect('postgres://epiqyfxlurqhrg:31bb627e0b611243bde68e6a095fa6c34f0ba9d9ca298c71f49dc06b29552f1f@ec2-52-1-95-247.compute-1.amazonaws.com:5432/dfum13t49qh7st', cursor_factory=DictCursor)
@@ -21,3 +24,15 @@ def get_db():
 
     return g.postgres_db_cur
 
+
+# Sqlite
+def connect_db():
+    sql = sqlite3.connect(PATH_TO_SQLITE3_DB)
+    sql.row_factory = sqlite3.Row
+    return sql
+
+def get_db():
+
+    if not hasattr(g, 'sqlite_db'):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
