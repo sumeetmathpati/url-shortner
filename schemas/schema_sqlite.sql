075d962c2819c7cb0bade7aa9d1dcad5449ed16f
Sumeet <sumeet221b@gmail.com>
2020-10-25 16:56:42 +0530

    updated sql query for sqlite3

--- schemas/schema_sqlite.sql
+++ schemas/schema_sqlite.sql
@@ -1,15 +1,15 @@
 CREATE TABLE users (
-    id int primary key autoincrement,
+    id integer primary key autoincrement,
     username text not null unique,
     password text not null,
     admin boolean not null
 );
 
 CREATE TABLE urls (
-    id int primary key autoincrement,
+    id integer primary key autoincrement,
     short_url text,
     long_url text ,
-    user_id int references users(id),
+    user_id integer references users(id),
     url_name text,
     unique(long_url, user_id)
-)
+);
